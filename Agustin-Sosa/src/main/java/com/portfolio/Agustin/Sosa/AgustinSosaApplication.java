f67db7eaef5f6dc36bcfb2265337f4d2e00dd431
Agustin-Sosa <agustinomarsosa@gmail.com>
2022-08-08 15:26:05 -0300

    creacion base de datos

---
+++ Agustin-Sosa/src/main/java/com/portfolio/Agustin/Sosa/AgustinSosaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Agustin.Sosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgustinSosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgustinSosaApplication.class, args);
+	}
+
+}
