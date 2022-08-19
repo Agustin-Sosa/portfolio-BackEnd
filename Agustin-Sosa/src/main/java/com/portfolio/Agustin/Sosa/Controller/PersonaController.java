983aec39b2a7c68a5aa04e8623e7a34c5f6f2ff5
Agustin-Sosa <agustinomarsosa@gmail.com>
2022-08-18 21:54:39 -0300

    actualizacion back end

--- Agustin-Sosa/src/main/java/com/portfolio/Agustin/Sosa/Controller/PersonaController.java
+++ Agustin-Sosa/src/main/java/com/portfolio/Agustin/Sosa/Controller/PersonaController.java
@@ -5,6 +5,8 @@ import com.portfolio.Agustin.Sosa.Entity.Persona;
 import com.portfolio.Agustin.Sosa.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -25,19 +28,22 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "La persona fue creada con éxito";
     }
     
-    @DeleteMapping("/personas/eliminar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/personas/eliminar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada con éxito";
     }
     
-    @PutMapping("/personas/editar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String newNombre,
                                @RequestParam("apellido") String newApellido,
@@ -50,6 +56,11 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
                                
 }
