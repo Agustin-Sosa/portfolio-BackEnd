dd6eebd2b0c5285a17e3a1386215d89da51b1a61
Agustin-Sosa <agustinomarsosa@gmail.com>
2022-10-19 11:33:05 -0300

    cc

--- Agustin-Sosa/src/main/java/com/portfolio/Agustin/Sosa/Security/Controller/AuthController.java
+++ Agustin-Sosa/src/main/java/com/portfolio/Agustin/Sosa/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontend-argprog-d7319.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
